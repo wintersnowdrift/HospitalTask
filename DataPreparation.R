1012881ba0520e3b603aae75c4cfdb2208e620ae
Kristin Svenson <kss@du.se>
2013-11-08 18:12:48 +0100

    Removed stuff I don't need from DataPreparation

--- DataPreparation.R
+++ DataPreparation.R
@@ -1,6 +1,6 @@
 # This file imports and prepares data
 rm(list=ls())
-source("multiplyDataWithRow.R")
+#source("multiplyDataWithRow.R") don't need it anymore
 
 # Network distances
 data <- read.table("//dustaff/home/kss/Documents/Complexity/PascalsProject/network distance.txt", 
@@ -18,11 +18,4 @@ pop <- read.table("//dustaff/home/kss/Documents/Complexity/PascalsProject/popula
 po <- pop[2]
 po <- as.numeric(as.character(po[,1]))
 
-# Multiply data with population
-dataPop <- multiplyDataWithRow(data,po)
-
-# Check data
-#dataPop[1:10,1:10]
-#data[1:10,1:10]
-#pop[1:10,2]
-# Data OK
+rm("pop")

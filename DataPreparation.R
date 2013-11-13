41590a1602b74e83db3b3a438a84db114c10b3e6
Kristin Svenson <kss@du.se>
2013-11-13 14:31:08 +0100

    Small changes to DataPreparation and OneHospital

--- DataPreparation.R
+++ DataPreparation.R
@@ -7,15 +7,15 @@ data <- read.table("//dustaff/home/kss/Documents/Complexity/PascalsProject/netwo
                    header=T)
 
 # Population
-pop <- read.table("//dustaff/home/kss/Documents/Complexity/PascalsProject/population.txt", 
+population <- read.table("//dustaff/home/kss/Documents/Complexity/PascalsProject/population.txt", 
                   header=T)
 
 # Check data
-#pop[1:10,]
+#population[1:10,]
 #data[1:10,]
 
 # Extract population as a single vector
-po <- pop[2]
+po <- population[2]
 po <- as.numeric(as.character(po[,1]))
 
 rm("pop")
--- OneHospital.R
+++ OneHospital.R
@@ -2,7 +2,12 @@
 source("DataPreparation.R")
 
 # Calculate column sums
+dataPop <- data*po
+dataPop[1:20,1:5]
+data[1:20,1:5]
+po[1:5]
 col.sums <- apply(dataPop, 2, sum)
+col.sums[1:5]
 locPop <- cbind(col.sums,node=seq(1:1579))
 locPop <- as.data.frame(locPop)
 locPop[1:20,]
@@ -13,6 +18,6 @@ which(locPop$col.sums == min(locPop$col.sums))
 locPop[742,]
 
 # Average distance per person
-locPop$col.sums[742]/277725
+(locPop$col.sums[742]/1000)/sum(po)
 
 # Same node as Meng but different distance

ddd4f76a9e8afed5feb148b68ffa45117199ba1a
Kristin Svenson <kss@du.se>
2013-11-13 14:32:07 +0100

    SA updated with "best"-condition

--- SimulatedAnnealingAlgorithm.R
+++ SimulatedAnnealingAlgorithm.R
@@ -1,4 +1,11 @@
-#Simulated annealing algorithm
+# Simulated annealing algorithm
+
+# network = network data set
+# population = vector with population numbers
+# p = number of hospitals to place
+# grannar = number of neigbours to pick
+# tmax = number of iterations
+
 sa <- function(network,population,p,grannar,tmax){
   emax <- -Inf #no solutions good enough
   
@@ -15,6 +22,8 @@ sa <- function(network,population,p,grannar,tmax){
   
   t <- tmax #to count the number of iterations
   
+  best <- s #the best solution so far
+  
   while(t>0 && e>emax){
     sn <- neighbour(s,N,grannar) #pick a neighbour
     en <- energy(sn,network,population) #cost of neigbour
@@ -23,11 +32,23 @@ sa <- function(network,population,p,grannar,tmax){
       s <- sn
       e <- en
     }
+    if(en<e){
+      best <- sn
+    }
     t <- t-1
   }
-  return(s)
+  # Calculate energy of best (i.e. the distance of this solution)
+  best_en <- energy(best,network,population)
+  
+  # Vector of nodes and energy as result
+  results <- c(best_en,best)
+  return(results)
 }
 
+# neighbour-function: picks a neighbour to the current solution
+# s = current solution
+# N = number of hospital nodes
+# grannar = number of neighbours to pick
 neighbour <- function(s,N,grannar){
   index <- sample(1:length(s),grannar,replace=FALSE)
   nygranne <- sample(1:N,grannar,replace=FALSE)
@@ -36,6 +57,7 @@ neighbour <- function(s,N,grannar){
   return(s)
 }
 
+# Test
 itShouldPickARandomNeighbour <- function(){
   s <- c(1,2,876,321,1)
   granne <- neighbour(s,1579,1)
@@ -47,6 +69,11 @@ itShouldPickARandomNeighbour <- function(){
 }
 
 itShouldPickARandomNeighbour()
+
+# energy-function: calculates the energy of the current solution
+# s = current solution
+# newtork = network data set
+# population = vector with population numbers
 
 energy <- function(s,network,population){
   #Go through every row and find which hosptital
@@ -71,6 +98,7 @@ P <- function(diffe,rt){
   return(fluff)
 }
 
+# Test
 itShouldReturnAValueBetween0and1 <- function(){
   value <- P(5,(9999/10000))
   return(value)
